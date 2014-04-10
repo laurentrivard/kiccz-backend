9342f5d4eab88cfa26b7ecb495fa8c42b4a2b8e5
Chris Splinter <csplinter@college.harvard.edu>
2014-04-10 14:38:26 -0400

    remove hard code

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ SECRET_KEY = 'kiccz_iphone_app_backend'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = os.environ['HEROKU_POSTGRESQL_TEAL_URL']
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
